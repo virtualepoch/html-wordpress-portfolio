50be5c5e29de3133ffc36bf73e4485cd55b50b07
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-12-23 14:19:59 -0700

    mobile button testing

---
+++ index.js
@@ -0,0 +1,62 @@
+// document.onload(insertNameTitle());
+// document.querySelector("body").addEventListener("onload", insertNameTitle());
+// setTimeout(function () {
+//     insertNameTitle();
+// }, 4000);
+
+function insertNameTitle() {
+    var name = "Craig Kaufman";
+    var title = "Front-End Web Developer";
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("title").innerHTML = title;
+    document.querySelector(".hello").classList.add("load");
+    document.querySelector(".hero-heading").classList.add("load");
+    document.querySelector(".hero-content").classList.add("load");
+}
+insertNameTitle();
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+
+window.onscroll = function () {
+    scroll();
+};
+
+function scroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("to-top-btn").classList.add("open");
+    } else {
+        document.getElementById("to-top-btn").classList.remove("open");
+    }
+}
+
+// OPEN CLOSE NAVIGATION MENU //////////////////////////////////
+const btnNavMenu = document.getElementById("btn-nav-menu");
+const navMenu = document.getElementById("nav-menu");
+
+function openCloseNavMenu() {
+    navMenu.classList.toggle("open");
+    btnNavMenu.classList.toggle("open");
+}
+
+
+///////////////////////////////////////////////////////////////////////
+// OPEN CLOSE CONTACT OVERLAY ////////////////////////////////////////
+
+const contactOverlay = document.querySelector(".contact-overlay-container")
+
+function openCloseContactOverlay() {
+    contactOverlay.classList.toggle('open');
+    contactOverlayScrollToTop();
+}
+
+const contactBtns = document.getElementsByClassName("btn-open-contact-overlay");
+
+for (let i = 0; i < contactBtns.length; i++) {
+    contactBtns[i].addEventListener("click", openCloseContactOverlay)
+}
+
+function contactOverlayScrollToTop() {
+    contactOverlay.scrollTo(0, 0);
+}
