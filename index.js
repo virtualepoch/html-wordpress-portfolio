8663171a73f05ed261d296e0da412eb39b980f01
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-12-24 15:52:32 -0700

    added swipe function

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 ///////////////////////////////////////////////////////////////////
 // DOCUMENT ON-LOAD FUNCTIONS ////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-
-// document.onload(onloadAddClass());
-// document.querySelector("body").addEventListener("onload", onloadAddClass());
-// setTimeout(function () {
-//     onloadAddClass();
-// }, 4000);
-
 function onloadAddClass() {
   document.querySelector(".hello").classList.add("load");
   document.querySelector(".hero-heading").classList.add("load");
@@ -19,12 +12,11 @@ onloadAddClass();
 ///////////////////////////////////////////////////////////////////
 // DOCUMENT SCROLL FUNCTIONS /////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-
 function scrollToTop() {
   window.scrollTo(0, 0);
 }
 
-function scroll() {
+function showScrollTopBtn() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("to-top-btn").classList.add("open");
   } else {
@@ -32,41 +24,25 @@ function scroll() {
   }
 }
 
-// OPEN/CLOSE PAGE ROWS
-
-// function pageContentRow1Open() {
-//   const scrollTest = 350;
-//   if (document.body.scrollTop >= scrollTest || document.documentElement.scrollTop >= scrollTest) {
-//     contentRows.classList.add("open");
-//   } else if (document.body.scrollTop <= scrollTest || document.documentElement.scrollTop <= scrollTest) {
-//     contentRows.classList.remove("open");
-//   }
-// }
-
 window.onscroll = function () {
-  scroll();
-  contentRow1Open();
-  // pageContentRow1Open();
+  showScrollTopBtn();
+  contentRowOpen();
 };
 
-// window.addEventListener("scroll", scroll, pageContentRow1Open);
-
 ///////////////////////////////////////////////////////////////////
-// ON VISIBILITY CHANGE FUNCTIONS /////////////////////////////////////
+// ON VISIBILITY CHANGE FUNCTIONS ////////////////////////////////
 /////////////////////////////////////////////////////////////////
-innerHeightRatio = 0.7;
 
 function isInViewPort(element) {
+  innerHeightRatio = 0.8;
+
   const rect = element.getBoundingClientRect();
   return rect.top <= (window.innerHeight * innerHeightRatio || document.documentElement.clientHeight * innerHeightRatio);
   // && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
 
-const contentRowContainers = document.getElementsByClassName("content-row-container");
-const contentRows = document.getElementsByClassName("content-row");
-
-function contentRow1Open() {
-  // element = contentRowContainers[0];
+function contentRowOpen() {
+  const contentRowContainers = document.getElementsByClassName("content-row-container");
 
   for (let i = 0; i < contentRowContainers.length; i++) {
     if (isInViewPort(contentRowContainers[i])) {
@@ -77,8 +53,9 @@ function contentRow1Open() {
   }
 }
 
+///////////////////////////////////////////////////////////////////
+// OPEN-CLOSE NAVIGATION MENU ////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-// OPEN CLOSE NAVIGATION MENU //////////////////////////////////
 const btnNavMenu = document.getElementById("btn-nav-menu");
 const navMenu = document.getElementById("nav-menu");
 
@@ -88,7 +65,8 @@ function openCloseNavMenu() {
 }
 
 /////////////////////////////////////////////////////////////////
-// OPEN CLOSE CONTACT OVERLAY //////////////////////////////////
+// OPEN-CLOSE CONTACT OVERLAY //////////////////////////////////
+///////////////////////////////////////////////////////////////
 
 const contactOverlay = document.querySelector(".contact-overlay-container");
 
@@ -106,3 +84,81 @@ const contactBtns = document.getElementsByClassName("btn-open-contact-overlay");
 for (let i = 0; i < contactBtns.length; i++) {
   contactBtns[i].addEventListener("click", openCloseContactOverlay);
 }
+
+///////////////////////////////////////////////////////////////////
+// SWIPE EVENTS //////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+window.addEventListener(
+  "load",
+  function () {
+    var touchsurface = document.querySelector(".contact-overlay-container"),
+      startX,
+      startY,
+      dist,
+      threshold = 150, //required min distance traveled to be considered swipe
+      allowedTime = 200, // maximum time allowed to travel that distance
+      elapsedTime,
+      startTime;
+
+    function handleswipe(isUpSwipe) {
+      if (isUpSwipe) touchsurface.classList.remove("open");
+    }
+
+    touchsurface.addEventListener(
+      "touchstart",
+      function (e) {
+        var touchobj = e.changedTouches[0];
+        dist = 0;
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime(); // record time when finger first makes contact with surface
+        // e.preventDefault();
+      },
+      false
+    );
+
+    touchsurface.addEventListener(
+      "touchmove",
+      function (e) {
+        e.preventDefault(); // prevent scrolling when inside DIV
+      },
+      false
+    );
+
+    touchsurface.addEventListener(
+      "touchend",
+      function (e) {
+        var touchobj = e.changedTouches[0];
+        dist = startY - touchobj.pageY; // get total dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime; // get time elapsed
+        // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+        var swiperightBol = elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageX - startX) <= 100;
+        handleswipe(swiperightBol);
+        // e.preventDefault();
+      },
+      false
+    );
+  },
+  false
+); // end window.onload
+
+// SCRAPPED FUNCTIONS ////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+// document.onload(onloadAddClass());
+// document.querySelector("body").addEventListener("onload", onloadAddClass());
+// setTimeout(function () {
+//     onloadAddClass();
+// }, 4000);
+
+// OPEN/CLOSE PAGE ROWS
+
+// function pageContentRow1Open() {
+//   const scrollTest = 350;
+//   if (document.body.scrollTop >= scrollTest || document.documentElement.scrollTop >= scrollTest) {
+//     contentRows.classList.add("open");
+//   } else if (document.body.scrollTop <= scrollTest || document.documentElement.scrollTop <= scrollTest) {
+//     contentRows.classList.remove("open");
+//   }
+// }
