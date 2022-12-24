2c1ea03d2426285654244e7d2b48a1b4d435a010
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-12-24 14:40:37 -0700

    slide animation testing

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-// document.onload(insertNameTitle());
-// document.querySelector("body").addEventListener("onload", insertNameTitle());
+///////////////////////////////////////////////////////////////////
+// DOCUMENT ON-LOAD FUNCTIONS ////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+// document.onload(onloadAddClass());
+// document.querySelector("body").addEventListener("onload", onloadAddClass());
 // setTimeout(function () {
-//     insertNameTitle();
+//     onloadAddClass();
 // }, 4000);
 
-function insertNameTitle() {
-  var name = "Craig Kaufman";
-  var title = "Front-End Web Developer";
-  document.getElementById("name").innerHTML = name;
-  document.getElementById("title").innerHTML = title;
+function onloadAddClass() {
   document.querySelector(".hello").classList.add("load");
   document.querySelector(".hero-heading").classList.add("load");
   document.querySelector(".hero-content").classList.add("load");
 }
-insertNameTitle();
+
+onloadAddClass();
+
+///////////////////////////////////////////////////////////////////
+// DOCUMENT SCROLL FUNCTIONS /////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 
 function scrollToTop() {
   window.scrollTo(0, 0);
@@ -27,23 +32,51 @@ function scroll() {
   }
 }
 
-const pageContentRow1 = document.querySelector(".content-row.row-1");
+// OPEN/CLOSE PAGE ROWS
 
-function pageContentRow1Open() {
-  const scrollTest = 300;
-  if (document.body.scrollTop >= scrollTest || document.documentElement.scrollTop >= scrollTest) {
-    pageContentRow1.classList.add("open");
-  } else if (document.body.scrollTop <= scrollTest || document.documentElement.scrollTop <= scrollTest) {
-    pageContentRow1.classList.remove("open");
-  }
-}
-
-// window.addEventListener("scroll", scroll, pageContentRow1Open);
+// function pageContentRow1Open() {
+//   const scrollTest = 350;
+//   if (document.body.scrollTop >= scrollTest || document.documentElement.scrollTop >= scrollTest) {
+//     contentRows.classList.add("open");
+//   } else if (document.body.scrollTop <= scrollTest || document.documentElement.scrollTop <= scrollTest) {
+//     contentRows.classList.remove("open");
+//   }
+// }
 
 window.onscroll = function () {
   scroll();
-  pageContentRow1Open();
+  contentRow1Open();
+  // pageContentRow1Open();
 };
+
+// window.addEventListener("scroll", scroll, pageContentRow1Open);
+
+///////////////////////////////////////////////////////////////////
+// ON VISIBILITY CHANGE FUNCTIONS /////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+innerHeightRatio = 0.7;
+
+function isInViewPort(element) {
+  const rect = element.getBoundingClientRect();
+  return rect.top <= (window.innerHeight * innerHeightRatio || document.documentElement.clientHeight * innerHeightRatio);
+  // && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
+
+const contentRowContainers = document.getElementsByClassName("content-row-container");
+const contentRows = document.getElementsByClassName("content-row");
+
+function contentRow1Open() {
+  // element = contentRowContainers[0];
+
+  for (let i = 0; i < contentRowContainers.length; i++) {
+    if (isInViewPort(contentRowContainers[i])) {
+      contentRowContainers[i].classList.add("open");
+    } else if (!isInViewPort(contentRowContainers[i])) {
+      contentRowContainers[i].classList.remove("open");
+    }
+  }
+}
+
 /////////////////////////////////////////////////////////////////
 // OPEN CLOSE NAVIGATION MENU //////////////////////////////////
 const btnNavMenu = document.getElementById("btn-nav-menu");
@@ -54,8 +87,8 @@ function openCloseNavMenu() {
   btnNavMenu.classList.toggle("open");
 }
 
-///////////////////////////////////////////////////////////////////////
-// OPEN CLOSE CONTACT OVERLAY ////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+// OPEN CLOSE CONTACT OVERLAY //////////////////////////////////
 
 const contactOverlay = document.querySelector(".contact-overlay-container");
 
@@ -64,12 +97,12 @@ function openCloseContactOverlay() {
   contactOverlayScrollToTop();
 }
 
+function contactOverlayScrollToTop() {
+  contactOverlay.scrollTo(0, 0);
+}
+
 const contactBtns = document.getElementsByClassName("btn-open-contact-overlay");
 
 for (let i = 0; i < contactBtns.length; i++) {
   contactBtns[i].addEventListener("click", openCloseContactOverlay);
-}
-
-function contactOverlayScrollToTop() {
-  contactOverlay.scrollTo(0, 0);
 }
